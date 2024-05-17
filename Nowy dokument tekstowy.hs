e5555c5f0008f25d7295c6ec04953be842bb418a
pawelmarszalekuwm <115549330+pawelmarszalekuwm@users.noreply.github.com>
2024-05-17 11:13:46 +0200

    Update Nowy dokument tekstowy.hs

--- Nowy dokument tekstowy.hs
+++ Nowy dokument tekstowy.hs
@@ -25,3 +25,21 @@ kwadrat x=x*x
 kwadrat_lista::[Int]->[Int]
 kwadrat_lista []=[]
 kwadrat_lista(x:xs) = (kwadrat x):(kwadrat_lista xs)
+--
+
+mniejsze x=x-1
+
+mniejsze_o1::[Int]->[Int]
+mniejsze_o1 []=[]
+mniejsze_o1(x:xs) = (mniejsze x):(mniejsze_o1 xs)
+--
+
+razy x=x*3
+
+razy3::[Int]->[Int]
+razy3 []=[]
+razy3(x:xs) = (razy x):(razy3 xs)
+
+dlugosc_listy::[Int]->Int
+dlugosc_listy[]=0
+dlugosc_listy(x:t)=1+(dlguosc_listy)
